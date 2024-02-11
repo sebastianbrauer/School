c0683af64513ba5608514f300e80368e6395a3ee
Sebastian Brauer <sebastianbrauer650@gmail.com>
2024-02-11 12:06:33 -0600

    Add Files

---
+++ GIS 5572/Lab 1 Part 2/main.py
@@ -0,0 +1,46 @@
+import os
+import psycopg2
+from flask import Flaskfrom dotenv import load_dotenv 
+load_dotenv()
+
+app = Flask(__name__)
+
+ 
+@app.route("/data", methods=["GET"]) 
+def data():
+    
+    conn = psycopg2.connect( 
+        host=os.environ.get("DB_HOST"), 
+        database=os.environ.get("DB_NAME"), 
+        user=os.environ.get("DB_USER"), 
+        password=os.environ.get("DB_PASSWORD"), 
+        port=os.environ.get("DB_PORT"), 
+    )
+    
+    with conn.cursor() as cur:
+        
+        query = """
+            SELECT 
+                jsonb_build_object(
+                    'type', 'FeatureCollection',
+                    'features', jsonb_agg(feature)
+                ) AS geojson
+            FROM (
+                SELECT 
+                    'Feature'::text AS type,
+                    ST_AsGeoJSON(geom)::jsonb AS geometry,
+                    '{}'::jsonb AS properties
+                FROM 
+                    your_table_name
+            ) AS feature;
+
+"""
+        cur.execute("SELECT ST_AsGeoJSON(my_table.*)::json FROM my_table;") 
+        data = cur.fetchall()
+        
+    conn.close() 
+     
+    return data
+
+if __name__ == "__main__":*/
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
